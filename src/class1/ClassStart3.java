52ce33bc93cb4656c354f0d98dbde759cff1263b
koreaioi <koreaioi@gachon.ac.kr>
2024-01-08 08:59:05 +0900

    객체 사용

--- src/class1/ClassStart3.java
+++ src/class1/ClassStart3.java
@@ -1,7 +1,13 @@
 package class1;
 
 public class ClassStart3 {
-
+    /*
+     * 객체사용 - . (dot) 사용
+     * 객체가 가진 멤버 변수에 접근하기 위해서는 먼저 객체에 접근해야한다.
+     * 변수(student1)에 들어있는 참조값(x001)을 읽어서 메모리에 존재하는 객체에 접근
+     * 그후 . (dot)을 사용해 멤버 변수들에 접근한다.
+     * student1.name -> x001.name -> "학생1"
+     * */
     public static void main(String[] args) {
         Student student1; //Student 타입을 받을 수 있는 변수 선언.
         /* new Student()로 객체 생성후 참조값 보관.
@@ -19,6 +25,8 @@ public class ClassStart3 {
 
         System.out.println("이름:" + student1.name + " 나이:" + student1.age + " 성적:" + student1.grade );
         System.out.println("이름:" + student2.name + " 나이:" + student2.age + " 성적:" + student2.grade );
+
+
 
     }
 }
